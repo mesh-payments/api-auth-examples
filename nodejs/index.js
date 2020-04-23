f69bb921ec41aed0cdb074b4aff385163f36552d
dying <inktechser@gmail.com>
2020-04-23 06:58:22 +0530

    better nonce generation for nodejs

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,38 +1,39 @@
-'use strict';
+"use strict";
 
-const crypto = require('crypto-js');
-const axios = require('axios');
+const crypto = require("crypto-js");
+const axios = require("axios");
+const uuid = require("uuid");
 
 const callApi = async (host, key, secret) => {
-    // gather parameters
-    const timestamp = new Date().toISOString();
-    const nonce = Math.floor(Math.random() * 5);
+  // gather parameters
+  const timestamp = new Date().toISOString();
+  const nonce = uuid.v4();
 
-    // generate signature
-    const payload = `date:${timestamp}\nx-mesh-nonce:${nonce}`;
-    const signature = crypto.HmacSHA256(payload, secret);
-    const signatureEncoded = crypto.enc.Base64.stringify(signature);
+  // generate signature
+  const payload = `date:${timestamp}\nx-mesh-nonce:${nonce}`;
+  const signature = crypto.HmacSHA256(payload, secret);
+  const signatureEncoded = crypto.enc.Base64.stringify(signature);
 
-    // call API
-    const auth = `HMAC-SHA256 Credential=${key};SignedHeaders=Date,x-mesh-nonce;Signature=${signatureEncoded}`;
-    const response = await axios.get(
-        `${host}/status`,
-        {
-            headers: {
-                Authorization: auth,
-                Date: timestamp,
-                'x-mesh-nonce': nonce
-            }
-        });
+  // call API
+  const auth = `HMAC-SHA256 Credential=${key};SignedHeaders=Date,x-mesh-nonce;Signature=${signatureEncoded}`;
+  const response = await axios.get(`${host}/status`, {
+    headers: {
+      Authorization: auth,
+      Date: timestamp,
+      "x-mesh-nonce": nonce,
+    },
+  });
 
-    console.log(`Request ID [${response.headers['x-amzn-requestid']}] returned [${response.status}]\n`, response.data);
+  console.log(
+    `Request ID [${response.headers["x-amzn-requestid"]}] returned [${response.status}]\n`,
+    response.data
+  );
 };
 
 const host = process.env.HOST;
 const key = process.env.API_KEY;
 const secret = process.env.API_SECRET;
 if (!host || !key || !secret) {
-    throw new Error('HOST, API_KEY or API_SECRET is missing');
+  throw new Error("HOST, API_KEY or API_SECRET is missing");
 }
 callApi(host, key, secret);
-
